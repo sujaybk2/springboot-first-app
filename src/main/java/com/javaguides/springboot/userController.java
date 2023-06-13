d848cbc802bf67dc56a5ca43d3a80cd36f546091
Sujay <sujaybk2@yahoo.com>
2023-06-13 14:38:30 -0700

    removed activeStatus

--- src/main/java/com/javaguides/springboot/userController.java
+++
@@ -1,7 +0,0 @@
-package com.javaguides.springboot;
-
-public class userController {
-    //connect to db and validate username and pw are the same
-    //inputs username and pw. return "successful" or "unsuccessful" using boolean
-    //post method should create username and pw
-}
